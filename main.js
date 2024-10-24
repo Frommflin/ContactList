4b073739daef439bc010dd394e6a8c7c9b6d6321
Klara Frommelin <freakyfrommelin@outlook.com>
2024-10-24 12:05:38 +0200

    Moving code into functions

--- main.js
+++ main.js
@@ -2,6 +2,10 @@ let contactId = 2;
 
 document.getElementById("newContact").addEventListener("submit", function(){ 
     event.preventDefault();
+    addNewContact();
+});
+
+function addNewContact(){
     contactId++;
     let str = ``;
     let name = document.querySelector("input[name='name']").value;
@@ -15,46 +19,37 @@ document.getElementById("newContact").addEventListener("submit", function(){
     str += `<input type="tel" name="phone${contactId}" value="${phone}" disabled>`;
     str += `</td>`;
     str += `<td>`;
-    str += `<button id="edit${contactId}" class="edit">Redigera</button>`;
-    str += `<button id="save${contactId}" class="save">Spara</button>`;
+    str += `<button id="edit${contactId}" class="edit" onclick="editContact(${contactId})">Redigera</button>`;
+    str += `<button id="save${contactId}" class="save" onclick="saveChanges(${contactId})">Spara</button>`;
     str += `</td>`;
     str += `<td>`;
-    str += `<button id="delete${contactId}" class="delete">Ta bort</button>`;
+    str += `<button id="delete${contactId}" class="delete" onclick="removeContact(${contactId})">Ta bort</button>`;
     str += `</td>`;
 
     document.getElementById("contacts").innerHTML += str;
     document.getElementById("newContact").reset();
-});
-
-let edits = document.getElementsByClassName("edit");
-for (let i = 0; i < edits.length; i++) {
-    document.getElementById("edit" + (i+1)).addEventListener("click", function(){
-        //Enable textfields relating to chosen contact
-        document.querySelector("input[name='name" + (i+1) + "']").disabled = false;
-        document.querySelector("input[name='phone" + (i+1) + "']").disabled = false;
-
-        //Switch visible button for editing
-        document.getElementById("edit"+(i+1)).style.display = "none";
-        document.getElementById("save"+(i+1)) .style.display = "block";
-    });
 }
 
-let saveEdits = document.getElementsByClassName("save");
-for (let i = 0; i < saveEdits.length; i++) {
-    document.getElementById("save" + (i+1)).addEventListener("click", function(){
-        //Disable textfields relating to chosen contact
-        document.querySelector("input[name='name" + (i+1) + "']").disabled = true;
-        document.querySelector("input[name='phone" + (i+1) + "']").disabled = true;
+function editContact(id){
+    //Enable textfields relating to chosen contact
+    document.querySelector("input[name='name" + id + "']").disabled = false;
+    document.querySelector("input[name='phone" + id + "']").disabled = false;
 
-        //Switch visible button for editing
-        document.getElementById("edit" + (i+1)).style.display = "block";
-        document.getElementById("save" + (i+1)) .style.display = "none";
-    });
+    //Switch visible button for editing
+    document.getElementById("edit"+id).style.display = "none";
+    document.getElementById("save"+id) .style.display = "block";
 }
 
-let deletes = document.getElementsByClassName("delete");
-for (let i = 0; i < deletes.length; i++) {
-    document.getElementById("delete" + (i+1)).addEventListener("click", function(){
-        document.getElementById("contact" + (i+1)).remove
-    });
+function saveChanges(id){
+    //Disable textfields relating to chosen contact
+    document.querySelector("input[name='name" + id + "']").disabled = true;
+    document.querySelector("input[name='phone" + id + "']").disabled = true;
+
+    //Switch visible button for editing
+    document.getElementById("edit" + id).style.display = "block";
+    document.getElementById("save" + id) .style.display = "none";
+}
+
+function removeContact(id){
+    document.getElementById("contact" + id).remove();
 }
